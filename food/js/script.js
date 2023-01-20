cec54eba9b5608f26dac452493507a70f2d11c47
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-01-20 23:57:15 +0200

    added form post to server

--- food/js/script.js
+++ food/js/script.js
@@ -217,9 +217,53 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const forms = document.querySelectorAll('form');
 
+    const message = {
+        loading: 'loading',
+        success: 'we connect to you',
+        failure: 'something wrong'
+    };
+
+forms.forEach(item =>{
+    postData(item);
+});
+
     function postData(form){
-        form.addEventListener('submit',()=>{
-             console.log('test');
+        form.addEventListener('submit',(e)=>{
+             e.preventDefault();
+
+             const statusMessage = document.createElement('div');
+             statusMessage.classList.add('status');
+             statusMessage.textContent = message.loading;
+             form.append(statusMessage);
+
+             const request = new XMLHttpRequest();
+             request.open('POST', '../server.php');
+
+             request.setRequestHeader('Content-type', 'application/json');
+             const formData = new FormData(form);
+
+             const object ={};
+             formData.forEach(function(value, key){
+                object[key] = value;
+             });
+
+             const json = JSON.stringify(object);
+
+             request.send(json);
+
+             request.addEventListener('load', ()=>{
+                if (request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(()=>{
+                        statusMessage.remove();
+                    },2000);
+                }else{
+                    statusMessage.textContent = message.failure; 
+                }
+             });
+
         });
     }
 
